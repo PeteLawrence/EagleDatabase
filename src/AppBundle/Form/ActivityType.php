0849aba6099e69ac0cdc67ae14f35732b352867d
Pete Lawrence <petejlawrence@icloud.com>
2016-08-23 13:30:08 +0100

    Activity CRUD

---
+++ src/AppBundle/Form/ActivityType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ActivityType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('activityStart', DateTimeType::class)
+            ->add('activityEnd', DateTimeType::class)
+            ->add('spaces')
+            ->add('signupStart', DateTimeType::class)
+            ->add('signupEnd', DateTimeType::class)
+            ->add('activityType')
+            ->add('organiser')
+            ->add('location')
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'AppBundle\Entity\Activity'
+        ));
+    }
+}
