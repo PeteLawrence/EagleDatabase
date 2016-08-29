c931f34e91d2c6f2665ac2bd060623104f202177
Pete Lawrence <petejlawrence@icloud.com>
2016-08-29 10:55:25 +0100

    More work on Signin form, including moving it to AppBundle and requiring a key instead of an admin session to access

---
+++ src/AppBundle/Controller/SignInController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use AppBundle\Entity\Activity;
+use AppBundle\Entity\Participant;
+
+class SignInController extends Controller
+{
+    /**
+     * Displays a sign-in page
+     *
+     * @Route("/signin/{id}", name="signin_show")
+     * @Method({"GET", "POST"})
+     */
+    public function signinAction(Request $request, Activity $activity)
+    {
+        //Check to see if the signin key is valid
+        $signInKey = $request->get('key');
+        if ($signInKey == null or $signInKey != $activity->getSigninKey()) {
+            throw $this->createAccessDeniedException('Sign In Key was invalid');
+        }
+
+
+        $form = $this->createSigninForm($activity);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $data = $form->getData();
+
+            $participant = new Participant();
+            $participant->setActivity($activity);
+            $participant->setParticipantRole($data['participantRole']);
+            $participant->setPerson($data['person']);
+
+            $em->persist($participant);
+            $em->flush();
+
+            $this->addFlash('notice', 'You have been signed in!');
+
+            return $this->redirectToRoute('signin_show', [ 'id' => $activity->getId(), 'key' => $activity->getSigninKey() ]);
+        }
+
+        return $this->render('signin/show.html.twig', array(
+            'activity' => $activity,
+            'signin_form' => $form->createView(),
+        ));
+    }
+
+
+    private function createSigninForm(Activity $activity)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('signin_show', array('id' => $activity->getId(), 'key' => $activity->getSigninKey())))
+            ->setMethod('POST')
+            ->add('person', EntityType::class, ['class' => 'AppBundle:Person', 'choice_label' => function ($a) { return $a->getForename() . ' ' . $a->getSurname(); }, 'placeholder' => '' ])
+            ->add('participantRole', EntityType::class, ['class' => 'AppBundle:ParticipantRole', 'choice_label' => 'role', 'label' => 'Role'])
+            ->getForm()
+        ;
+    }
+}
