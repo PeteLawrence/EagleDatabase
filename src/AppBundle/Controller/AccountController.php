6e713f8bae6080382c59252557b66b443856296c
Pete Lawrence <petejlawrence@icloud.com>
2016-09-23 20:45:24 +0100

    Added start of account edit page

--- src/AppBundle/Controller/AccountController.php
+++ src/AppBundle/Controller/AccountController.php
@@ -5,6 +5,10 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 /**
  * @Route("/account")
@@ -17,8 +21,60 @@ class AccountController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('account/overview.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        return $this->render('account/overview.html.twig');
+    }
+
+
+    /**
+     * @Route("/edit", name="account_edit")
+     */
+    public function editAction(Request $request)
+    {
+        $user = $this->get('security.token_storage')->getToken()->getUser();
+        $form = $this->buildNameForm($user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //Save the changes
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('notice', 'Your changes were saved!');
+
+            return $this->redirectToRoute('account_edit');
+        }
+
+        return $this->render(
+            'account/edit.html.twig',
+            [
+                'user' => $user,
+                'nameForm' => $form->createView()
+            ]
+        );
+    }
+
+
+
+    public function buildNameForm($user)
+    {
+        return $this->createFormBuilder($user)
+            ->setAction($this->generateUrl('account_edit'))
+            ->setMethod('POST')
+            ->add('forename', TextType::class, [ 'attr' => ['placeholder' => 'Forename(s)'] ])
+            ->add('surname', TextType::class, [ 'attr' => ['placeholder' => 'Surname'] ])
+            ->add('email', EmailType::class, [ 'attr' => ['placeholder' => 'Email Address'] ])
+            ->add('gender', ChoiceType::class, [ 'choices' => [ 'Female' => 'F', 'Male' => 'M'] ])
+            ->add('dob', BirthdayType::class, [ 'widget' => 'choice', 'label' => 'D.o.B', 'format' => 'd MMM y', 'years' => range(1910, date('Y')) ])
+            ->add('addr1', TextType::class, [ 'attr' => ['placeholder' => 'Address Line 1'] ])
+            ->add('addr2', TextType::class, [ 'attr' => ['placeholder' => 'Address Line 2'] ])
+            ->add('town', TextType::class, [ 'attr' => ['placeholder' => 'Town'] ])
+            ->add('county', TextType::class, [ 'attr' => ['placeholder' => 'County'] ])
+            ->add('postcode', TextType::class, [ 'attr' => ['placeholder' => 'Postcode'] ])
+            ->add('telephone', TextType::class, [ 'attr' => ['placeholder' => 'Phone Number'] ])
+            ->add('mobile', TextType::class, [ 'attr' => ['placeholder' => 'Mobile Number'] ])
+            ->getForm()
+        ;
     }
 }
