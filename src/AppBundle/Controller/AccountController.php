b62964a1a21d22fec5c99147d5471d9ba960c50d
Pete Lawrence <petejlawrence@icloud.com>
2016-11-26 20:50:01 +0000

    Rebuilt renew process to enable selection of extras and summary before paying

--- src/AppBundle/Controller/AccountController.php
+++ src/AppBundle/Controller/AccountController.php
@@ -9,8 +9,11 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use AppBundle\Form\StripePaymentForm;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\HttpFoundation\Session\Session;
+use AppBundle\Entity\MemberRegistration;
 
 /**
  * @Route("/account")
@@ -53,25 +56,31 @@ class AccountController extends Controller
     public function membershipRenewAction(Request $request)
     {
         //Get available membership options
-        $renewForm = $this->buildStripePaymentForm();
+        $form = $this->buildMembershipTypeForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $request->getSession()->set('renew_mtp', $data['membershipTypePeriod']->getId());
+
+            return $this->redirectToRoute('account_membership_renew2');
+        }
 
         // replace this example code with whatever you need
         return $this->render(
             'account/renew.html.twig',
             [
-                'renewForm' => $renewForm->createView(),
-                'stripe_publishable_key' => $this->getParameter('stripe.publishable_key')
+                'form' => $form->createView()
             ]
         );
     }
 
-
-    private function buildStripePaymentForm()
+    private function buildMembershipTypeForm()
     {
         return $this->createFormBuilder()
-            ->setAction($this->generateUrl('stripe_callback'))
             ->setMethod('POST')
-            ->add('membershipType',
+            ->add('membershipTypePeriod',
                 EntityType::class,
                 [
                     'class' => \AppBundle\Entity\MembershipTypePeriod::class,
@@ -86,6 +95,153 @@ class AccountController extends Controller
                         );
                     }
                 ])
+            ->getForm();
+    }
+
+    /**
+     * @Route("/membership/renew2", name="account_membership_renew2")
+     */
+    public function membershipRenew2Action(Request $request)
+    {
+        $em = $this->get('doctrine')->getManager();
+        $session = $request->getSession();
+
+        $mtp = $em->getRepository('AppBundle:MembershipTypePeriod')->findOneById($session->get('renew_mtp'));
+
+        //Get available membership options
+        $form = $this->buildMembershipExtrasForm($mtp);
+
+
+        if ($request->getMethod() == 'POST') {
+            $extras = [];
+            $data = $request->request->get('form');
+            foreach ($data as $field => $enabled) {
+                if ($field == '_token') {
+                    continue;
+                }
+
+                $extras[] = substr($field, 6);
+            }
+            $session->set('renew_extras', $extras);
+
+            return $this->redirectToRoute('account_membership_renew3');
+        }
+
+
+        // replace this example code with whatever you need
+        return $this->render(
+            'account/renew2.html.twig',
+            [
+                'form' => $form->createView()
+            ]
+        );
+    }
+
+
+    private function buildMembershipExtrasForm($membershipTypePeriod)
+    {
+        $fb = $this->createFormBuilder()
+            ->setMethod('POST');
+
+        //Loop over each of the extras available given $membershipTypePeriod
+        foreach ($membershipTypePeriod->getMembershipTypePeriodExtra() as $extra) {
+            $fb->add(
+                'extra_' . $extra->getId(),
+                CheckboxType::class,
+                [
+                    'label' => sprintf(
+                        '£%s - %s (%s)',
+                        number_format($extra->getValue(), 2),
+                        $extra->getMembershipExtra()->getName(),
+                        $extra->getMembershipExtra()->getDescription()
+                    )
+                ]
+            );
+        }
+
+        return $fb->getForm();
+    }
+
+
+    /**
+     * @Route("/membership/renew3", name="account_membership_renew3")
+     */
+    public function membershipRenew3Action(Request $request)
+    {
+        $em = $this->get('doctrine')->getManager();
+        $session = $request->getSession();
+
+        $total = 0;
+
+        //Create the MemberRegistration record
+        $membershipTypePeriod = $em->getRepository('AppBundle:MembershipTypePeriod')->findOneById($session->get('renew_mtp'));
+        $memberRegistration = new MemberRegistration();
+        $memberRegistration->setMembershipTypePeriod($membershipTypePeriod);
+        $total += $membershipTypePeriod->getPrice();
+
+        foreach ($session->get('renew_extras') as $extraId) {
+            $extra = $em->getRepository('AppBundle:MembershipTypePeriodExtra')->findOneById($extraId);
+            $membershipRegistrationExtra = new \AppBundle\Entity\MemberRegistrationExtra;
+            $membershipRegistrationExtra->setMembershipTypePeriodExtra($extra);
+            $membershipRegistrationExtra->setMemberRegistration($memberRegistration);
+            $memberRegistration->addMemberRegistrationExtra($membershipRegistrationExtra);
+
+            $total += $extra->getValue();
+        }
+
+
+        //Get available membership options
+        $form = $this->buildStripePaymentForm();
+
+        $form->handleRequest($request);
+        dump($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            //Configure Stripe
+             \Stripe\Stripe::setApiKey($this->getParameter('stripe.secret_key'));
+
+            // Get the credit card details submitted by the form
+            $token = $_POST['stripeToken'];
+
+            // Create a charge: this will charge the user's card
+            try {
+                $charge = \Stripe\Charge::create(array(
+                    "amount" => ($total * 100), // Amount in pence
+                    "currency" => "gbp",
+                    "source" => $token,
+                    "description" => "Membership"
+                ));
+            } catch (\Stripe\Error\Card $e) {
+                // The card has been declined
+                throw new \Exception('There was an error taking your payment.');
+            }
+
+            $memberRegistration->setRegistrationDateTime(new \DateTime());
+            $memberRegistration->setPerson($this->getUser());
+            $em->persist($memberRegistration);
+            $em->flush();
+
+            $this->addFlash('notice', 'Your renewal has been successful');
+
+            return $this->redirectToRoute('account_overview');
+        }
+
+        // replace this example code with whatever you need
+        return $this->render(
+            'account/renew3.html.twig',
+            [
+                'form' => $form->createView(),
+                'memberRegistration' => $memberRegistration,
+                'stripe_publishable_key' => $this->getParameter('stripe.publishable_key'),
+                'total' => $total
+            ]
+        );
+    }
+
+    private function buildStripePaymentForm()
+    {
+        return $this->createFormBuilder()
+            ->setMethod('POST')
             ->getForm();
     }
 
