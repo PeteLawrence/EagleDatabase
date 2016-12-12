55d1184511965b1bc7a4787085cfc074d027866e
Pete Lawrence <petejlawrence@icloud.com>
2016-12-12 18:56:38 +0000

    ChargeRepository fix

--- src/AppBundle/Entity/Charge.php
+++ src/AppBundle/Entity/Charge.php
@@ -5,7 +5,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ORM\Entity
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\ChargeRepository")
  * @ORM\InheritanceType("JOINED")
  * @ORM\DiscriminatorColumn(name="type", type="string")
  * @ORM\DiscriminatorMap({
