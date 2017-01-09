a5954dda381c0df7def99c115ca36727d369d715
Pete Lawrence <petejlawrence@icloud.com>
2017-01-09 13:06:23 +0000

    Fix decimal problem on Location long/lat fields

--- src/AppBundle/Entity/Location.php
+++ src/AppBundle/Entity/Location.php
@@ -30,12 +30,12 @@ class Location
     private $postcode;
 
     /**
-     * @ORM\Column(type="decimal", nullable=true)
+     * @ORM\Column(type="decimal", nullable=true, precision=6)
      */
     private $longitude;
 
     /**
-     * @ORM\Column(type="decimal", nullable=true)
+     * @ORM\Column(type="decimal", nullable=true, precision=6)
      */
     private $latitude;
 
