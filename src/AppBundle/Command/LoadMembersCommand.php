47a1bc28efa3fd848fcfa8e9fe198c69d2eaff78
Pete Lawrence <petejlawrence@icloud.com>
2017-03-04 12:50:05 +0000

    Fix importing

--- src/AppBundle/Command/LoadMembersCommand.php
+++ src/AppBundle/Command/LoadMembersCommand.php
@@ -25,39 +25,38 @@ class LoadMembersCommand extends ContainerAwareCommand
         //Read in the first line of headers
         fgetcsv($fh);
 
+
         while (($data = fgetcsv($fh, 1000, ",")) !== false) {
+            var_dump($data);
             printf('%s %s', $data[1], $data[2]);
             $person = new \AppBundle\Entity\Person;
             $person->setForename($data[1]);
             $person->setSurname($data[2]);
-            $person->setEmail($data[12]);
+            $person->setEmail($data[11]);
             $person->setAdmin(false);
-            $person->setPassword(null);
-            if ($data[13] != 'NULL') {
-                $person->setDob(new \DateTime($data[13]));
+            $person->setPassword('X'); //set to X as can't be null
+            if ($data[12] != 'NULL') {
+                $person->setDob(new \DateTime($data[12]));
             }
             $person->setAddr1($data[4]);
             $person->setAddr2($data[5]);
-            $person->setAddr2($data[6]);
-            $person->setTown($data[7]);
-            $person->setCounty($data[8]);
-            $person->setPostcode($data[9]);
-            $person->setTelephone($data[10]);
-            $person->setMobile($data[11]);
+            $person->setTown($data[6]);
+            $person->setCounty($data[7]);
+            $person->setPostcode($data[8]);
+            $person->setTelephone($data[9]);
+            $person->setMobile($data[10]);
             $person->setDisability(false);
-            $person->setNotes($data[17]);
+            $person->setNotes($data[16]);
 
-            $person->setGender($data[49]);
+            $person->setGender($data[45]);
 
             $person->setIsActive(true);
 
-            $person->setNextOfKinName($data[20]);
-            $person->setNextOfKinContactDetails($data[21]);
-            $person->setNextOfKinRelation($data[22]);
+            $person->setNextOfKinName($data[19]);
+            $person->setNextOfKinContactDetails($data[20]);
+            $person->setNextOfKinRelation($data[21]);
 
             $person->setNotes($data[17]);
-
-            $person->setPassword('X'); //set to X as can't be null
 
             $em->persist($person);
         }
