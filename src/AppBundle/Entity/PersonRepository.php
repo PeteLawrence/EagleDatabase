42d8f10dda851f2b1edf7ab6afd11ecc7e3afcea
Pete Lawrence <petejlawrence@icloud.com>
2017-04-11 20:48:56 +0100

    Added youth members report

--- src/AppBundle/Entity/PersonRepository.php
+++ src/AppBundle/Entity/PersonRepository.php
@@ -30,6 +30,36 @@ class PersonRepository extends EntityRepository
     }
 
 
+    public function findMembersByType($type, $date = null)
+    {
+        if ($date == null) {
+            $date = new \DateTime();
+        }
+
+        return $this->queryMembersByType($type, $date)
+            ->getQuery()
+            ->getResult();
+
+    }
+
+
+    public function queryMembersByType($type, $date)
+    {
+        $q = $this->createQueryBuilder('p')
+            ->innerJoin('p.memberRegistration', 'mr')
+            ->innerJoin('mr.membershipTypePeriod', 'mtp')
+            ->innerJoin('mtp.membershipPeriod', 'mp')
+            ->where('?1 BETWEEN mp.fromDate AND mp.toDate')
+            ->andWhere('mr.registrationDateTime <= ?1')
+            ->andWhere('mtp.membershipType = ?2')
+            ->orderBy('p.forename')
+            ->addOrderBy('p.surname')
+            ->setParameter(1, $date)
+            ->setParameter(2, $type);
+        return $q;
+    }
+
+
     public function findMembersByEmailAndDob($email, $dob)
     {
         $query = $this->createQueryBuilder('p')
@@ -41,4 +71,5 @@ class PersonRepository extends EntityRepository
 
         return $query->getQuery()->getResult();
     }
+
 }
