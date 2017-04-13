7a325f4055691644e5e47bbb1bc472b6b8908116
Pete Lawrence <petejlawrence@icloud.com>
2017-04-13 20:03:50 +0100

    Added registration date time field to MemberRegistrationType.php

--- src/AppBundle/Form/Type/MemberRegistrationType.php
+++ src/AppBundle/Form/Type/MemberRegistrationType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use AppBundle\Entity\Person;
 use AppBundle\Entity\MembershipTypePeriod;
 
@@ -20,7 +21,7 @@ class MemberRegistrationType extends AbstractType
         $builder
             ->add('person', EntityType::class, ['class' => 'AppBundle:Person', 'choice_label' => function (Person $a) { return $a->getForename() . ' ' . $a->getSurname(); }, ])
             ->add('membershipTypePeriod', EntityType::class, ['class' => 'AppBundle:MembershipTypePeriod', 'choice_label' => function (MembershipTypePeriod $a) { return sprintf('%s %s → %s £%s', $a->getMembershipType()->getType(), $a->getMembershipPeriod()->getFromDate()->format('d-m-Y'), $a->getMembershipPeriod()->getToDate()->format('d-m-Y'), $a->getPrice()); } ])
-
+            ->add('registrationDateTime', DateTimeType::class, [ 'required' => false, 'date_format' => 'd MMMM y' ])
         ;
     }
 
