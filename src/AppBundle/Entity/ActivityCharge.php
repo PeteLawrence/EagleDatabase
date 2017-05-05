db8ab93a323e5bec56362e1bc49afcbc3223f76e
Pete Lawrence <petejlawrence@icloud.com>
2017-05-05 22:16:59 +0100

    Add in support for charging members when signing up online

--- src/AppBundle/Entity/ActivityCharge.php
+++ src/AppBundle/Entity/ActivityCharge.php
@@ -7,4 +7,33 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class ActivityCharge extends \AppBundle\Entity\Charge
 {
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\ManagedActivity", inversedBy="activityCharge")
+     * @ORM\JoinColumn(name="managed_activity_id", referencedColumnName="id")
+     */
+    private $managedActivity;
+
+    /**
+     * Set managedActivity
+     *
+     * @param \AppBundle\Entity\ManagedActivity $managedActivity
+     *
+     * @return ActivityCharge
+     */
+    public function setManagedActivity(\AppBundle\Entity\ManagedActivity $managedActivity = null)
+    {
+        $this->managedActivity = $managedActivity;
+
+        return $this;
+    }
+
+    /**
+     * Get managedActivity
+     *
+     * @return \AppBundle\Entity\ManagedActivity
+     */
+    public function getManagedActivity()
+    {
+        return $this->managedActivity;
+    }
 }
