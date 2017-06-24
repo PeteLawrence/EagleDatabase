65775c995697ec20c61727051b51b46d8062ce55
Pete Lawrence <petejlawrence@icloud.com>
2017-06-24 08:45:25 +0100

    Log last logged in time

---
+++ src/AppBundle/EventListener/LoginListener.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\EventListener;
+
+use Symfony\Component\Security\Core\Event\AuthenticationEvent;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+
+class LoginListener {
+
+    private $em;
+
+    public function __construct(\Doctrine\ORM\EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
+    public function onSecurityInteractiveLogin(InteractiveLoginEvent $event)
+    {
+        //Set the lastLoginDateTime on the user
+        $user = $event->getAuthenticationToken()->getUser();
+        $user->setLastLoginDateTime(new \DateTime());
+
+        //FLush changes to DB
+        $this->em->flush();
+    }
+
+}
