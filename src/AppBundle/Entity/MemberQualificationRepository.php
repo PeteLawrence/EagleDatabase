50c44b6c1a7e2c926868300d3272194020f9af46
Pete Lawrence <petejlawrence@icloud.com>
2017-07-01 19:36:15 +0100

    Added expiring and expired qualification reports

---
+++ src/AppBundle/Entity/MemberQualificationRepository.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+class MemberQualificationRepository extends EntityRepository
+{
+    /**
+     * Returns activities that a person has attended/signed up for
+     *
+     * @param  [type] $person [description]
+     *
+     * @return [type]         [description]
+     */
+    public function findExpiringQualifications()
+    {
+        $now = new \DateTime();
+        $limit = clone $now;
+        $limit->sub(new \DateInterval('P3M'));
+
+        $q = $this->createQueryBuilder('mq')
+            ->where('mq.validTo < ?1')
+            ->andWhere('mq.validTo > ?2')
+            ->setParameter(1, $limit)
+            ->setParameter(2, $now)
+            ->orderBy('mq.validTo');
+
+        return $q->getQuery()->getResult();
+    }
+
+
+    /**
+     * Returns activities that a person has attended/signed up for
+     *
+     * @param  [type] $person [description]
+     *
+     * @return [type]         [description]
+     */
+    public function findExpiredQualifications()
+    {
+        $now = new \DateTime();
+
+        $q = $this->createQueryBuilder('mq')
+            ->where('mq.validTo < ?1')
+            ->setParameter(1, $now)
+            ->orderBy('mq.validTo');
+
+        return $q->getQuery()->getResult();
+    }
+}
